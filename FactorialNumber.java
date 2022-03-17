a913a268557b92ed758809456929c3ac9e73c0a0
17019298 <101631440+17019298@users.noreply.github.com>
2022-03-17 15:52:11 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,21 @@
+package Basiccommands;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i,factorial=1,n=5;
+	       for (i=1;i<=5;i++)
+	      {
+	          
+	    	   factorial=factorial*i;
+	          
+	    	   System.out.println(factorial);
+	    	 
+	         }
+	           
+	}
+
+}
