c7e1e9ab63103a2bd7b7cef79219b720139111ab
17019298 <101631440+17019298@users.noreply.github.com>
2022-03-17 16:36:25 +0530

    Add files via upload

---
+++ breakS.java
@@ -0,0 +1,20 @@
+package Basiccommands;
+
+public class breakS {
+
+	public static void main(String[] args) {
+      int i,n=1;
+       for (i=1;i<=5;i++)
+       {
+          
+       
+          
+    	   System.out.println(n*i*i*i);
+    	 
+          }
+           
+       
+    	 
+    	   }}
+
+
